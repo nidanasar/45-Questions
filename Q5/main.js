4a07fe60f887384c5e7c236d3127ee0c64f1dee2
Nida Muhammad Nasar <nidashafqet@gmail.com>
2024-05-09 04:07:09 +0500

    Add files via upload

---
+++ Q5/main.js
@@ -0,0 +1,4 @@
+"use strict";
+let famous_person = "Muhammad Ali Jinnah";
+let message = "there is no power on earth that can undo pakistan";
+console.log(`${famous_person} Once said "${message}"`);
---
+++ Q5/main.ts
@@ -0,0 +1,6 @@
+// Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. 
+// Then compose your message and store it in a new variable called message. Print your message.
+
+let famous_person:string="Muhammad Ali Jinnah";
+let message:string= "there is no power on earth that can undo pakistan";
+console.log(`${famous_person} Once said "${message}"`)
