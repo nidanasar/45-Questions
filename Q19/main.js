9d991d600d19279d7a10aa09107c4641359fb51a
Nida Muhammad Nasar <nidashafqet@gmail.com>
2024-05-09 04:33:21 +0500

    Add files via upload

---
+++ Q19/main.js
@@ -0,0 +1,4 @@
+"use strict";
+let invitations = ["Nida", "Anaya"];
+let countInvitaions = invitations.length;
+console.log(`${countInvitaions} people will come to the dinner`);
---
+++ Q19/main.ts
@@ -0,0 +1,7 @@
+// Dinner Guests: Working with one of the programs from Exercises 14 through 18, 
+// print a message indicating the number of people you are inviting to dinner.
+
+
+let invitations:string[]=["Nida","Anaya"]
+let countInvitaions:number=invitations.length
+console.log(`${countInvitaions} people will come to the dinner`); 
