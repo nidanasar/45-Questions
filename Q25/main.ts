f201acef20b56a32711448a28c8c84f4eb756814
Nida Muhammad Nasar <nidashafqet@gmail.com>
2024-05-09 04:42:26 +0500

    Add files via upload

---
+++ Q25/main.ts
@@ -0,0 +1,16 @@
+// create a vaiable called Alien Colors
+let alien_color:string='Green';
+
+//  Write an if statement to test whether the alien’s color is green.If it is,
+// print a message that the player just earned 5 points.
+if(alien_color==='Green'){
+console.log("player just earned 5 points.!")
+}
+
+//Write one version of this program that passes the if test and another that fails. 
+alien_color='Yellow';
+
+//(The version that fails will have no output.)
+if (alien_color==='green'){
+console.log("player just earned 5 points !")
+}
