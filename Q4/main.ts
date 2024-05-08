3756dc1a2e9b7e2420635a45de4cd300a9b37ea8
Nida Muhammad Nasar <nidashafqet@gmail.com>
2024-05-09 03:25:17 +0500

    Add files via upload

---
+++ Q4/main.ts
@@ -0,0 +1,7 @@
+// Famous Quote: Find a quote from a famous person you admire. 
+// Print the quote and the name of its author. Your output should look something like the following, 
+// including the quotation marks:Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+
+
+let author:string= "Muhammad Ali Jinnah";
+console.log(`${author} Once said,"there is no power on earth that can UNDO Pakistan"`)
