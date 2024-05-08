6e21b04a293ed05f6d4cdded53b544cdea592a23
Nida Muhammad Nasar <nidashafqet@gmail.com>
2024-05-09 04:54:14 +0500

    Add files via upload

---
+++ Q36/main.js
@@ -0,0 +1,10 @@
+"use strict";
+// T-Shirt: Write a function called make_shirt() that accepts a size and the text of a message that should be printed
+//  on the shirt. The function should print a sentence summarizing the size of the shirt and the message printed on it. 
+//  Call the function.
+function make_shirt(size, text) {
+    console.log(`${size} T-shirt with message, ${text}`);
+}
+make_shirt('large', 'I LOVE PAKISTAN');
+make_shirt('Meduim', 'I LOVE PAKISTAN');
+make_shirt('Samll', 'I LOVE PAKISTAN');
