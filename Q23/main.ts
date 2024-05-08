01ee799dbafded956619e4980a290367bd7ef798
Nida Muhammad Nasar <nidashafqet@gmail.com>
2024-05-09 04:39:06 +0500

    Add files via upload

---
+++ Q23/main.ts
@@ -0,0 +1,42 @@
+// conditional tests
+
+let car='subaru';
+//test 1
+console.log("is car =='subaru'? i predict true.")
+console.log(car=='subaru')
+
+//test 2
+console.log("is car != 'honda city'? i predict true")
+console.log(car != 'honda city')
+
+// test 3
+console.log("is car =='subaru'? i predict true")
+console.log(car == 'subaru')
+
+// test 4
+console.log("is car == 'SUBARU'? i predict false")
+console.log(car=="SUBARU")  
+
+//test 5
+console.log("is car .length == 6? predict true")
+console.log(car.length==6)
+
+// test 6
+console.log("is car.length != 10? predict true")
+console.log(car.length != 10)
+
+// test 7
+console.log("is 10 >50 i predicet false")
+console.log(10 > 50)
+
+//test 8
+console.log("is 9<=6  i predict false")
+console.log(9<=6)
+
+// test 9
+console.log("is 72 >= 83? i predict false")
+console.log(72>=83)
+
+// test 10
+console.log("is car =='subaru' && car.length ==3? i predict false")
+console.log(car == 'subaru' && car.length == 3)
