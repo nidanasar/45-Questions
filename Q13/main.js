9216ff7c257d0a34b54b6e027c36fb0417da5672
Nida Muhammad Nasar <nidashafqet@gmail.com>
2024-05-09 04:19:49 +0500

    Add files via upload

---
+++ Q13/main.js
@@ -0,0 +1,5 @@
+"use strict";
+let vegetables = ['onion', 'garlic', 'ginger'];
+vegetables.map((index) => console.log(`i like ${index}`));
+let transpotaions = ['honda', 'civic', 'audi', 'suzuki'];
+//transpotaions.map((items)=>console.log(`i would like to own a ${items}`));
---
+++ Q13/main.ts
@@ -0,0 +1,7 @@
+// Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car,
+//  and make a list that stores several examples. Use your list to print a series of statements about these items,
+//   such as “I would like to own a Honda motorcycle.”
+
+
+let transpotaions:string[]=['honda','civic','audi','suzuki'];
+transpotaions.map((items)=>console.log(`i would like to own a ${items}`));
