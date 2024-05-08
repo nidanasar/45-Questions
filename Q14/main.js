96f15d177a2af41993b50846e775a2847baaf694
Nida Muhammad Nasar <nidashafqet@gmail.com>
2024-05-09 04:26:49 +0500

    Add files via upload

---
+++ Q14/main.js
@@ -0,0 +1,3 @@
+"use strict";
+let guestList = ['Nida', 'Anaya', 'Anoosha'];
+guestList.map((index) => console.log(`${index},i would like to invite you on dinner tomorrow `));
---
+++ Q14/main.ts
@@ -0,0 +1,7 @@
+// Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that 
+// includes at least three people you’d like to invite to dinner. Then use your list to print a message to each person, 
+// inviting them to dinner.
+
+
+let guestList:string[]=['Nida','Anaya','Anoosha'];
+guestList.map((index)=>console.log(`${index},i would like to invite you on dinner tomorrow `))
